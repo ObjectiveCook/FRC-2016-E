589731c49a11986499b77387d5ac38835972c758
Ryan <ObjectiveCook@gmail.com>
2016-03-24 13:40:02 -0400

    Update Vision.java

--- src/main/subsystems/Vision.java
+++ src/main/subsystems/Vision.java
@@ -1,5 +1,6 @@
 package main.subsystems;
 
+import java.io.IOException;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
@@ -7,15 +8,38 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
  *
  */
 public class Vision extends Subsystem {
-	private NetworkTable table;
-	private double[] centerx, centery, width, height, defaultValue = new double[0];
-
-	public Vision() {
-
-	}
+	private NetworkTable vision;
+	private double[] CenterX, CenterY, Width, Height, defaultValue = new double[0];
+	public double centerX, centerY, width, height;
 
 	public void initDefaultCommand() {
-		// Set the default command for a subsystem here.
-		// setDefaultCommand(new MySpecialCommand());
+		setDefaultCommand(null);
+	}
+	
+	public Vision() {
+		vision = NetworkTable.getTable("vision");
+		
+		try {
+            		new ProcessBuilder("/home/lvuser/grip").inheritIO().start();
+        	} catch (IOException e) {
+            		e.printStackTrace();
+        	}
+	}
+	
+	public boolean createImage() {
+        	CenterX = vision.getNumberArray("centerX", defaultValue);
+        	CenterY = vision.getNumberArray("centerY", defaultValue);
+        	Width = table.getNumberArray("width", defaultValue);
+		Height = table.getNumberArray("height", defaultValue);
+		
+		if (CenterX.length == 1 && CenterX[0] != null) {
+			centerX = centerx[0];
+			centerY = centery[0];
+			width = width[0];
+			height = height[0];
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
